6e7dc8258b01af11c4d41a8afd7d7dec68ab60e9
Strongo997 <vxtl1n13@soton.ac.uk>
2017-11-22 23:24:34 +0000

    Update request.php
    Add a test validation for post request

--- utility/request.php
+++ utility/request.php
@@ -4,7 +4,10 @@ class Request {
 
 	public function __construct() {
 		$this->type = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'get';
-		$this->data = $_POST;
+		foreach ($_POST as $name=>$value) {
+			if is_string($name) {htmlspecialchars($name);}
+		}
+		$this->data=$_POST;
 	}
 
 	public function is($type) {
