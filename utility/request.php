51c6d3d27a2355501af95c4643666f05ed134b59
Strongo997 <vxtl1n13@soton.ac.uk>
2017-11-23 12:21:22 +0000

    Update request.php
    added codes for validating user inputs

--- utility/request.php
+++ utility/request.php
@@ -4,11 +4,20 @@ class Request {
 
 	public function __construct() {
 		$this->type = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'get';
-		/** Not working yet */
+		/** check and validate each input from the user before pass them into the database */
+		$verified = array();
 		foreach ($_POST as $name=>$value) {
-			if is_string($name) {htmlspecialchars($name);}
+			if (is_string($value)) {
+				$value = stripslashes($value);
+				$value = str_replace('<','',$value);
+				$value = str_replace('>','',$value);
+				$value = str_replace('/','',$value);
+				$value = str_replace("'",'',$value);
+				$value = htmlspecialchars($value);
+				$verified[$name] = $value;
+				}
 		}
-		$this->data=$_POST;
+		$this->data=$verified;
 	}
 
 	public function is($type) {
